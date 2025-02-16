a5d49115b5e91a3de6d6a82d88aa6e0aaee83057
Matthias <m@tth.es>
2025-02-16 11:27:38 +0100

    sync

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,5 +9,13 @@ $app->registerClass(\Lack\Kindergarden\Coder\Coder::class);
 $app->registerClass(\Lack\Kindergarden\Coder\BL\CoderPrepare::class);
 $app->registerClass(\Lack\Kindergarden\Coder\BL\CoderRun::class);
 
+if (class_exists(\Lack\Keystore\KeyStore::class)) {
+    \Lack\Kindergarden\Kindergarden::addKey(\Lack\Keystore\KeyStore::Get()->getAccessKey("open_ai"));
+} elseif (getenv("OPENAI_API_KEY") !== false) {
+    \Lack\Kindergarden\Kindergarden::addKey("open_ai", getenv("OPENAI_API_KEY"));
+} else {
+    throw new \Exception("No OpenAI API key found. Please provide it in the environment variable 'OPENAI_API_KEY' or by installing the lack/keystore package.");
+}
 
-\Lack\Kindergarden\Kindergarden::addKey(\Lack\Keystore\KeyStore::Get()->getAccessKey("open_ai"));
+
+
