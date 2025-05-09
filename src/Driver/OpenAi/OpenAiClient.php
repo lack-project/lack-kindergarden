7d178c23f7a11e3087aae7a9bad83d383c1dd052
Matthias <m@tth.es>
2025-05-09 08:05:09 +0200

    update

--- src/Driver/OpenAi/OpenAiClient.php
+++ src/Driver/OpenAi/OpenAiClient.php
@@ -6,7 +6,7 @@ class OpenAiClient
 {
 
     const OPENAI_DEFAULT_MODEL = "chatgpt-4o-latest";
-    const OPENAI_DEFAULT_REASONING_MODEL = "o4-mini";
+    const OPENAI_DEFAULT_REASONING_MODEL = "o3";
 
 
     private array $requests = [];
